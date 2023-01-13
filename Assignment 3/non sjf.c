ee539ee089fe790f02d51241c6933edfb4511c22
AnuragSS <anuragshete01@gmail.com>
2023-01-13 11:28:18 +0530

    Update non sjf.c

--- Assignment 3/non sjf.c
+++ Assignment 3/non sjf.c
@@ -1,155 +1,95 @@
-#include<stdio.h>
 #include<string.h>
-#include<stdlib.h>
-struct process
+#include<stdio.h>
+struct job
 {
-	int first_bt,temp_bt,next_bt,at,ft,tat,wt;
-}
-p[100];
-struct schedule
-{
-	int end_time,pid;
-}sch[100];
-int sch_cnt;  //schedule counter
-int ct;  // current time
-int np;	 // Number of process
-int i;
+	char name[20];
+	int at,bt,ct,tat,wt,st,tbt;
+}job[10];
+
+int i,j,n;
+float avg_tat=0;
+float avg_wt=0;
 void take_input()
 {
-	printf("Enter the number of process :  ");
-	scanf("%d",&np);
-	
-	
-	for(i=0;i<np;i++)
+	printf("\n Enter the no. of processes:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
 	{
-		printf("Enter the arival time of the proess %d : ",i);
-		scanf("%d",&p[i].at);
+		printf("Enter the arrival time of the process%d",i);
+		scanf("%d",&job[i].at);
 	}
-	printf("\n");
-	for(i=0;i<np;i++)
+	for(i=0;i<n;i++)
 	{
-		printf("Enter the burst time of the process %d : ",i);
-		scanf("%d",&p[i].first_bt);
-		p[i].temp_bt = p[i].first_bt;
+		printf("Enter the burst time of the process %d",i);
+		scanf("%d",&job[i].bt);
+		job[i].tbt=job[i].bt;
 	}
-}
-
-void take_rinput()
-{
-	for(i=0;i<np;i++)
+	for(i=0;i<n;i++)
 	{
-		p[i].next_bt = rand()%10+1;
-		p[i].temp_bt = p[i].next_bt;
+		printf("Enter the name of the process %d",i);
+		scanf("%s",&job[i].name);
 	}
 }
 
-int getProcessBySJF()
+void sort()
 {
-	int minp = -1;
-	int minat = 32768;
-
-	for(i=0;i<np;i++)
+		struct job temp;
+	for(i=0;i<n;i++)
 	{
-		if(p[i].at<=ct && p[i].temp_bt >0 && p[i].temp_bt<minat)
+		for(j=i+1;j<n;j++)
 		{
-			minp = i;
-			minat = p[i].temp_bt;
+			if(job[i].bt>job[j].bt)
+			{
+				temp=job[i];
+				job[i]=job[j];
+				job[j]=temp;
+			}
 		}
 	}
-	return minp;
 }
-
-void showGanttChart()
+void process_by_sjf()
 {
-	for(i=0;i<sch_cnt*5;i++)
-		printf("-");
-	printf("\n|");
-
-	for(i=0;i<sch_cnt;i++)
-		printf("P%d |",sch[i].pid);
-	printf("\n");
-
-	for(i=0;i<sch_cnt*5;i++)
-		printf("-");
-	printf("\n0");
-
-	for(i=0;i<sch_cnt;i++)
-		printf("%5d",sch[i].end_time);
-}
-
-void print_output()
-{
-	float avgtat = 0,avgwt =0;
-	printf("\n");
-	printf("\n PID \tFBT \t RBT \t AT \t FT \t TAT \t WT \n");
-	for(i=0;i<np;i++)
+	int time=job[0].bt;
+	for(j=0;j<n;j++)
 	{
-		printf("\nP%d \t %d \t %d \t %d \t %d \t %d \t %d\n",i,p[i].first_bt,p[i].next_bt,p[i].at,p[i].ft,p[i].tat,p[i].wt);
-		
-	
-	
-	avgtat = avgtat + p[i].tat;
-	avgwt  = avgwt  + p[i].wt;
+		job[j].st=time;
+		printf(" | %d %s",job[j].st,job[j].name);
+		time=time+job[j].tbt;
+		job[j].ct=time;
+		job[j].tat=time-job[j].bt;
+		job[j].wt=job[j].tat-job[j].tbt;
+		printf("%d |",time);
 	}
-		
-	printf("Average  turn around time : %f",(float)avgtat/np);
-	printf("\nAverage waiting time : %f\n",(float)avgwt/np);
+}
+void print_output1()
+{
+	printf("\n \n");
+	printf("\n-------------------------------------");
+	printf("\nPname         BT           TAT            WT");
+	printf("\n-------------------------------------");
+	for(i=0;i<n;i++)
+	{
+		printf("\n%s        %8d           %5d             %4d             %3d",job[i].name,job[i].bt,job[i].tat,job[i].wt);
+		avg_tat=avg_tat+(float)(job[i].tat);
+		avg_wt=(float)avg_wt+(float)(job[i].wt);
+	}
+	printf("\n--------------------------------------");
 }
 
-void main()
+
+int main()
 {
+	int i;
 	take_input();
-	//to process first burst time of each process
-	while(1)
+	sort();
+	process_by_sjf();
+	print_output1();
+	printf("\n\n");
+	for(i=0;i<n;i++)
 	{
-		int x = getProcessBySJF();
-		if(x==-1)    // if no process, will return -1
-			break;
-		sch[sch_cnt].pid = x;
-		ct += p[x].temp_bt;
-		p[x].temp_bt = 0;
-		p[x].ft = ct;
-		sch[sch_cnt].end_time = ct;
-		sch_cnt++;
-		p[x].tat = p[x].ft-p[x].at;
-		p[x].wt = p[x].tat-p[x].first_bt;
+		job[i].tbt=job[i].bt=rand()%10+1;
+		job[i].at=job[i].ct+2;
 	}
-	showGanttChart();
-	print_output();
-	
-	/*take_rinput();
-	
-	//To process Next Burst Time of each process
-     
-	     while(1)
-		{
-			int x = getProcessBySJF();
-			if(x==-1)    // if no process, will return -1
-				break;
-			sch[sch_cnt].pid = x;
-			ct += p[x].temp_bt;
-			p[x].temp_bt = 0;
-			p[x].ft = ct;
-			sch[sch_cnt].end_time = ct;
-			sch_cnt++;
-			p[x].tat = p[x].ft-p[x].at;
-			p[x].wt = p[x].tat-p[x].first_bt-P[x].next_bt-2;
-		}
-		showGanttChart();
-		print_output();
-		
-	*/	
+	process_by_sjf();
+	print_output1();
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
